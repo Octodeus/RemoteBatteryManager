cff3ad627a679cb126b883865d150c9b29333edb
Joonas Känsälä <jj.kansala@gmail.com>
2017-06-14 11:17:11 +0300

    Changed hello world text content programmatically. I have now lost my Kotlin virginity

--- RemoteBatteryMonitor/app/src/main/java/com/example/joonas/remotebatterymonitor/MainActivity.kt
+++ RemoteBatteryMonitor/app/src/main/java/com/example/joonas/remotebatterymonitor/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.joonas.remotebatterymonitor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val label1 = findViewById(R.id.firstLabel) as TextView
+        label1.text = "Sup world!"
     }
+
 }
